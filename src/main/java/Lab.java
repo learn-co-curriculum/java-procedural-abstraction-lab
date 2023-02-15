bc5f2db54e88ea80ea81f77248ab8d16521d1df7
Katie Cleland <kcleland0818@gmail.com>
2023-02-14 20:31:26 -0700

    Write lab

---
+++ src/main/java/Lab.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+public class Lab {
+
+    public static void main(String[] args) {
+        // Assume we are going to a fair!
+
+        System.out.println("Let's go to the fair!");
+
+        // Section One - Uncomment this out when you get to this section
+//        rideSwings();
+//        rideFerrisWheel(2);
+
+        // Section Two - Uncomment this out when you get to this section
+//        playGame();
+
+        // Section Three - Uncomment this out when you get to this section
+//        double totalPrice = buyFunnelCake();
+//        totalPrice += buyCornDogs(3);
+//        System.out.printf("Spent $%.2f on fair food!", totalPrice);
+
+    }
+
+    // Write the rideSwings() method
+
+    // Write the rideFerrisWheel() method
+
+    // Write the playGame() method
+
+    // Write the buyFunnelCake() method
+
+    // Write the buyCorndogs() method
+}
